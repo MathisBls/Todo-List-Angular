c553fd56ce2b901c12e4a2bbcbc11da1b5bd72b2
Mathis Boulais <mathis.oxym@gmail.com>
2025-09-01 16:24:25 +0200

    feat: setup basic app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AUTH_ROUTES } from './features/auth/auth.routes';
+import { ADMIN_ROUTES } from './features/admin/admin.routes';
+import { TODOS_ROUTES } from './features/todos/todos.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: AUTH_ROUTES,
+  },
+  {
+    path: 'admin',
+    children: ADMIN_ROUTES,
+  },
+  {
+    path: 'todos',
+    children: TODOS_ROUTES,
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+];
