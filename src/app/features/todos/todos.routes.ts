f9f2b1386faecf3eb00cc80bbc1b70b74706c6ce
Mathis Boulais <mathis.oxym@gmail.com>
2025-09-03 09:48:04 +0200

    feat: delete todo list component

--- src/app/features/todos/todos.routes.ts
+++ src/app/features/todos/todos.routes.ts
@@ -6,8 +6,4 @@ export const TODOS_ROUTES: Routes = [
     path: '',
     loadComponent: () => import('./components/todos.component').then(m => m.TodosComponent),
   },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./components/todo-list.component').then(m => m.TodoListComponent),
-  },
 ];
